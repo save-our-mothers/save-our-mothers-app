03aa1dcf864ee254aacfaef1a31f58c9466c8b5e
Glonel Dimapilis <gdimapilis@aol.com>
2023-06-21 18:47:17 -0500

    setup reducer.js, saga.js, and installed MUI, eCharts, and chartJS

---
+++ src/redux/reducers/chartReducers.reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+const chartReducers = combineReducers({
+  // Reducers go here
+
+});
+
+export default chartReducers;
---
+++ src/redux/sagas/chartSagas.saga.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { put, takeLatest, takeEvery } from 'redux-saga/effects';
+
+function* chartSaga() {
+  // Sagas go here
+
+}
+
+export default chartSaga;
