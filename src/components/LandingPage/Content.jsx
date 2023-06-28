f63fe2b081d8fe1f809fb71cdbd45200050e5769
Glonel Dimapilis <gdimapilis@aol.com>
2023-06-28 17:58:45 -0500

    Added a Content component to hold/display each graph, as well as working on functionality to popup the graph in a more detailed view.

--- src/components/LandingPage/Content.jsx
+++ src/components/LandingPage/Content.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useEffect } from 'react';
 
-function Content() {
-
+function Content({ windowStatus, setWindowStatus, chartType, setChartType }) {
+  console.log(windowStatus)
 
   return (
 
     <>
-      <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
-      <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
-      <p>I'm testing the size and height. It can probably be changed later,
-        but this seems pretty good from what I can tell.
-      </p>
+      <div>
+        <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
+        <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
+        <p>I'm testing the size and height. It can probably be changed later,
+          but this seems pretty good from what I can tell.
+        </p>
+      </div>
     </>
   )
 }
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -11,10 +11,14 @@ import Content from './Content.jsx'
 
 function LandingPage() {
   const [heading, setHeading] = useState('Safe Childbirth Foundation');
+  const [chartType, setChartType] = useState('');
+  const [windowStatus, setWindowStatus] = useState(false);
   const history = useHistory();
 
-  const expandWindow = () => {
-
+  const expandWindow = (e, type) => {
+    setWindowStatus(!windowStatus);
+    console.log(`windowStatus changed`)
+    console.log(`value: `, type)
   }
 
   return (
@@ -25,37 +29,42 @@ function LandingPage() {
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={2} sm={4} md={4}>
             <p>Age Graph</p>
-            <div className="view-border" onClick={expandWindow}>
-              <Content />
-            </div>
+              <div className="view-border" onClick={(e) => expandWindow(e, 'age')} >
+                <Content
+                  windowStatus={windowStatus}
+                  setWindowStatus={setWindowStatus}
+                  chartType={chartType}
+                  setChartType={setChartType}
+                />
+              </div>
           </Grid>
           <Grid item xs={2} sm={4} md={4}>
             <p>Gender Graph</p>
-            <div className="view-border">
+            <div className="view-border" onClick={(e) => expandWindow(e, 'gender')} >
               <Content />
             </div>
           </Grid>
           <Grid item xs={2} sm={4} md={4}>
             <p>Family Size Graph</p>
-            <div className="view-border">
+            <div className="view-border" onClick={(e) => expandWindow(e, 'family-size')} >
               <Content />
             </div>
           </Grid>
           <Grid item xs={2} sm={4} md={4}>
             <p>Top 10 Prescriptions Graph</p>
-            <div className="view-border">
+            <div className="view-border" onClick={(e) => expandWindow(e, 'prescriptions')} >
               <Content />
             </div>
           </Grid>
           <Grid item xs={2} sm={4} md={4}>
             <p>Geo Location Map</p>
-            <div className="view-border">
+            <div className="view-border" onClick={(e) => expandWindow(e, 'map')} >
               <Content />
             </div>
           </Grid>
           <Grid item xs={2} sm={4} md={4}>
             <p>Number of Patients Chart</p>
-            <div className="view-border">
+            <div className="view-border" onClick={(e) => expandWindow(e, '#-of-patients')} >
               <Content />
             </div>
           </Grid>
