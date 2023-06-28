bb5c733fb182e1d0be4a452b8693ef995ff86da9
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-06-27 20:38:00 -0400

    added database creation queries to database.sql

--- database.sql
+++ database.sql
@@ -9,3 +9,39 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "prescriptions" (
+	"id" SERIAL PRIMARY KEY,
+	"drug_name" VARCHAR NOT NULL
+);
+
+CREATE TABLE "ages" (
+	"id" SERIAL PRIMARY KEY,
+	"range" VARCHAR NOT NULL,
+	"count" INT NOT NULL
+);
+
+CREATE TABLE "locations" (
+	"id" SERIAL PRIMARY KEY,
+	"neighborhood" VARCHAR NOT NULL,
+	"city" VARCHAR NOT NULL,
+	"count" INT NOT NULL
+);
+
+CREATE TABLE "patients_unique" (
+	"id" SERIAL PRIMARY KEY,
+	"count" INT NOT NULL,
+	"gender" VARCHAR NOT NULL
+);
+
+CREATE TABLE "patient_visits" (
+	"id" SERIAL PRIMARY KEY,
+	"count" INT NOT NULL,
+	"type" VARCHAR NOT NULL
+);
+
+CREATE TABLE "family_size" (
+	"id" SERIAL PRIMARY KEY,
+	"range" VARCHAR NOT NULL,
+	"count" INT NOT NULL
+);
