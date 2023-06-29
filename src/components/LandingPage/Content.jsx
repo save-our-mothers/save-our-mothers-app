8f27a4c444df6b3e8fb9e39b9730e209c03c9901
Glonel Dimapilis <gdimapilis@aol.com>
2023-06-28 23:02:38 -0500

    Some more extra styling on hover over the Grid.

--- src/components/LandingPage/Content.jsx
+++ src/components/LandingPage/Content.jsx
@@ -13,75 +13,76 @@ function Content() {
   const closeWindow = () => {
     if (windowStatus === true) {
       dispatch({ type: 'TOGGLE_WINDOW_STATUS' });
-    } 
+    }
     dispatch({ type: 'SET_CHART_TYPE', payload: '' });
   }
 
   return (
-
-    <div className="popup">
-      <Button color="secondary" className="close-btn" onClick={closeWindow}>close</Button>
-      {
-        chartType === 'age' ? (
-          <div className="content-div">
-            <h3>Age</h3>
-            <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
-            <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
-            <p>I'm testing the size and height. It can probably be changed later,
-              but this seems pretty good from what I can tell.
-            </p>
-          </div>
-        ) : chartType === 'gender' ? (
-          <div className="content-div">
-            <h3>Gender</h3>
-            <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
-            <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
-            <p>I'm testing the size and height. It can probably be changed later,
-              but this seems pretty good from what I can tell.
-            </p>
-          </div>
-        ) : chartType === 'family-size' ? (
-          <div className="content-div">
-            <h3>Family Size</h3>
-            <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
-            <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
-            <p>I'm testing the size and height. It can probably be changed later,
-              but this seems pretty good from what I can tell.
-            </p>
-          </div>
-        ) : chartType === 'prescriptions' ? (
-          <div className="content-div">
-            <h3>Top 10 Prescription</h3>
-            <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
-            <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
-            <p>I'm testing the size and height. It can probably be changed later,
-              but this seems pretty good from what I can tell.
-            </p>
-          </div>
-        ) : chartType === 'map' ? (
-          <div className="content-div">
-            <h3>Geo Location Map</h3>
-            <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
-            <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
-            <p>I'm testing the size and height. It can probably be changed later,
-              but this seems pretty good from what I can tell.
-            </p>
-          </div>
-        ) : chartType === '#-of-patients' ? (
-          <div className="content-div">
-            <h3>Number of Patients</h3>
-            <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
-            <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
-            <p>I'm testing the size and height. It can probably be changed later,
-              but this seems pretty good from what I can tell.
-            </p>
-          </div>
-        ) : (
-          <div className="content-div">
-            <h2>Please Select a View</h2>
-          </div>
-        )
-      }
+    <div className="popup-whole">
+      <div className="popup">
+        {
+          chartType === 'age' ? (
+            <div className="content-div">
+              <h3>Age</h3>
+              <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
+              <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
+              <p>I'm testing the size and height. It can probably be changed later,
+                but this seems pretty good from what I can tell.
+              </p>
+            </div>
+          ) : chartType === 'gender' ? (
+            <div className="content-div">
+              <h3>Gender</h3>
+              <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
+              <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
+              <p>I'm testing the size and height. It can probably be changed later,
+                but this seems pretty good from what I can tell.
+              </p>
+            </div>
+          ) : chartType === 'family-size' ? (
+            <div className="content-div">
+              <h3>Family Size</h3>
+              <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
+              <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
+              <p>I'm testing the size and height. It can probably be changed later,
+                but this seems pretty good from what I can tell.
+              </p>
+            </div>
+          ) : chartType === 'prescriptions' ? (
+            <div className="content-div">
+              <h3>Top 10 Prescription</h3>
+              <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
+              <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
+              <p>I'm testing the size and height. It can probably be changed later,
+                but this seems pretty good from what I can tell.
+              </p>
+            </div>
+          ) : chartType === 'map' ? (
+            <div className="content-div">
+              <h3>Geo Location Map</h3>
+              <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
+              <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
+              <p>I'm testing the size and height. It can probably be changed later,
+                but this seems pretty good from what I can tell.
+              </p>
+            </div>
+          ) : chartType === '#-of-patients' ? (
+            <div className="content-div">
+              <h3>Number of Patients</h3>
+              <p>lorem ipsum calorum pursuant unpontia torentia munis. sup erca lifra gilist egexpia lidos hus.</p>
+              <p>caloris fruignei moris brontix munis. famigla ageis genda cise.</p>
+              <p>I'm testing the size and height. It can probably be changed later,
+                but this seems pretty good from what I can tell.
+              </p>
+            </div>
+          ) : (
+            <div className="content-div">
+              <h2>Please Select a View</h2>
+            </div>
+          )
+        }
+      </div>
+      <Button color="secondary" variant="outlined" size="small" className="close-btn" onClick={closeWindow}>close</Button>
     </div>
   )
 }
