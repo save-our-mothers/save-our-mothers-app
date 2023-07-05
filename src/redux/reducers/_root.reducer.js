69d9bbe834261244e5b6e347c9fc82c866f64d07
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2023-07-05 14:02:47 -0500

    fixing

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import chartReducers from './chartReducers.reducer';
+import landingPageReducers from './landingPageReducers.reducer';
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -10,8 +10,9 @@ import chartReducers from './chartReducers.reducer';
 // This is what we get when we use 'state' inside of 'mapStateToProps'
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
-  user,
-  chartReducers, // will have an id and username if someone is logged in
+  user, // will have an id and username if someone is logged in
+  landingPageReducers, // Added for Landing Page variables -gd
+
 });
 
 export default rootReducer;
