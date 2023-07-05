58616e59e9b30442c75fd053c248efca2bc57417
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-07-05 10:36:03 -0400

    added response.js to use a test data

--- server/routes/source.router.js
+++ server/routes/source.router.js
@@ -2,19 +2,34 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 const axios = require('axios');
+const emrResponse = require('../response');
+const pool = require('../modules/pool');
+let data = {};
 
-router.get('/', (req, res) => {
-    axios.get(`https://saveourmothers.azurewebsites.net/api/openemr?code=${process.env.api_key}`)
-        .then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        }).catch(error => {
-            console.log(`Error in GET: ${error}`);
-            res.sendStatus(500);
+router.get('/', async (req, res) => {
+    // axios.get(`https://saveourmothers.azurewebsites.net/api/openemr?code=${process.env.api_key}`)
+    //     .then(response => {
+    //         console.log(response.data);
+    //         axios.post('/', response.data);
+    //     }).catch(error => {
+    //         console.log(`Error in GET: ${error}`);
+    //         res.sendStatus(500);
+    //     });
+    try {
+        data = emrResponse;
+        axios.post('/', (req, res) => {
+            res.sendStatus(200);
         });
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 });
 
 
+router.post('/', (req, res) => {
+    console.log('in POST request', req.body);
+})
 
 
 
