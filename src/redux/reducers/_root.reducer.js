c4346b0454cfcb9517de163afa546a32072920b9
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2023-07-05 14:03:37 -0500

    fixing

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import landingPageReducers from './landingPageReducers.reducer';
+
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
