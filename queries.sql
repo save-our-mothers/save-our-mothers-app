383a143ab244e52d739f3a6de4686e45f059a639
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-07-05 17:35:52 -0500

    add date dimension table SQL and SQL to get visits

--- queries.sql
+++ queries.sql
@@ -38,4 +38,68 @@ FROM `patient_data`;
 
 
 -- Query for getting patient visits
--- Will likely come from 'patient_tracker_element' - include @ and ~ for arrived and arrived late
+-- Given a date dimension table `date_d` shown below, this returns all appointments
+-- with a status of 'arrived' or 'arrived late' along with the year, date, week of year, month of year,
+-- and quarter. 
+SELECT date_d.year, `apptdate`, date_d.week_of_year, date_d.month_of_year, date_d.quarter
+FROM `patient_tracker`
+JOIN `patient_tracker_element` ON `id` = `pt_tracker_id` AND `status` = '@' OR '~'
+JOIN `date_d` ON `apptdate` = date_d.date
+ORDER BY date_d.year ASC;
+
+
+--! This would be for creating a dimension table in their DB that we could refer to but it's massive.
+
+-- DROP TABLE IF EXISTS numbers_small;
+-- CREATE TABLE numbers_small (number INT);
+-- INSERT INTO numbers_small VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9);
+
+-- -- Main-numbers table
+-- DROP TABLE IF EXISTS numbers;
+-- CREATE TABLE numbers (number BIGINT);
+-- INSERT INTO numbers
+-- SELECT thousands.number * 1000 + hundreds.number * 100 + tens.number * 10 + ones.number
+-- FROM numbers_small thousands, numbers_small hundreds, numbers_small tens, numbers_small ones
+-- LIMIT 1000000;
+
+-- -- Create Date Dimension table
+-- DROP TABLE IF EXISTS date_d;
+-- CREATE TABLE date_d (
+-- date_id          BIGINT PRIMARY KEY,
+-- date             DATE NOT NULL,
+-- year             INT,
+-- month            CHAR(10),
+-- month_of_year    CHAR(2),
+-- day_of_month     INT,
+-- day              CHAR(10),
+-- day_of_week      INT,
+-- weekend          CHAR(10) NOT NULL DEFAULT "Weekday",
+-- day_of_year      INT,
+-- week_of_year     CHAR(2),
+-- quarter  INT,
+-- previous_day     date NOT NULL default '0000-00-00',
+-- next_day         date NOT NULL default '0000-00-00',
+-- UNIQUE KEY `date` (`date`));
+
+-- -- First populate with ids and Date
+-- -- Change year start and end to match your needs. The above sql creates records for year 2010.
+-- INSERT INTO date_d (date_id, date)
+-- SELECT number, DATE_ADD( '2014-01-01', INTERVAL number DAY )
+-- FROM numbers
+-- WHERE DATE_ADD( '2014-01-01', INTERVAL number DAY ) BETWEEN '2020-01-01' AND '2025-12-31'
+-- ORDER BY number;
+
+-- -- Update other columns based on the date.
+-- UPDATE date_d SET
+-- year            = DATE_FORMAT( date, "%Y" ),
+-- month           = DATE_FORMAT( date, "%M"),
+-- month_of_year   = DATE_FORMAT( date, "%m"),
+-- day_of_month    = DATE_FORMAT( date, "%d" ),
+-- day             = DATE_FORMAT( date, "%W" ),
+-- day_of_week     = DAYOFWEEK(date),
+-- weekend         = IF( DATE_FORMAT( date, "%W" ) IN ('Saturday','Sunday'), 'Weekend', 'Weekday'),
+-- day_of_year     = DATE_FORMAT( date, "%j" ),
+-- week_of_year    = DATE_FORMAT( date, "%V" ),
+-- quarter         = QUARTER(date),
+-- previous_day    = DATE_ADD(date, INTERVAL -1 DAY),
+-- next_day        = DATE_ADD(date, INTERVAL 1 DAY);
